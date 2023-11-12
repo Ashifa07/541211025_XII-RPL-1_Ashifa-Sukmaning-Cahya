69974376645ad5d54580918dd10c70a2e446ce72
Ashifa <ashifasukmaningcahya@gmail.com>
2023-11-12 21:26:59 +0700

    part 14-17

--- router/users.js
+++ router/users.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const usercontroller = require('../controllers/users')
 
-const usercontroller = require('../controllers/user')
 
-router.get('/users', )
 
 router.get('/users', usercontroller.index)
 
-router.post('/user', usercontroller.store)
+router.get('/user/:id', usercontroller.show)
 
-router.put('/user/:id', usercontroller.update)
+  router.post('/user', usercontroller.store)
+
+  router.put('/user/:id', usercontroller.update)
 
   router.delete('/user/:id', usercontroller.delete)
+  
   module.exports = router
 
